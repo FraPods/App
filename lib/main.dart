7ad1cebe7f3e4713d4464184245da12f4c26b6d4
ceteruinam <yuzheng.liu00110@gmail.com>
2022-03-26 15:55:01 +0100

    theming and upload page bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -25,23 +25,45 @@ class FraPodsApp extends StatelessWidget {
           title: 'FraPods',
           themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
           theme: ThemeData(
-            appBarTheme: AppBarTheme(
-              backgroundColor: blueish()
-            ),
-            
-            brightness: Brightness.light,
-            primarySwatch: generateMaterialColorFromColor(Color(0xFF004AAD)),
-            primaryColor: Colors.black,
+            appBarTheme: AppBarTheme( backgroundColor: blueish(), foregroundColor: Colors.white),
+            // primaryColor: Colors.white,
+             colorScheme:ColorScheme.light(
+              primary: Color(0xff468CE8), 
+              primaryVariant: Color(0xFF99C4FD),
+              secondary: Color(0xFF424242), 
+              secondaryVariant: Color(0xff646464), 
+              surface: Color(0xffFFFEF6), 
+              background: Colors.white, 
+              error: Color(0xffb00020), 
+              onPrimary:Colors.black,
+              onSecondary: Colors.black, 
+              onSurface: Colors.black, 
+              onBackground: Colors.black, 
+              onError: Colors.white, 
+              brightness: Brightness.light,
+            )
           ),
 
           darkTheme: ThemeData(
-            appBarTheme: AppBarTheme(
-              backgroundColor: blueish()
-            ),
-            brightness: Brightness.dark,
-            primarySwatch: generateMaterialColorFromColor(Color(0xFF004AAD)),
-            primaryColor: Colors.white,
-            backgroundColor: backgroundColor()
+            appBarTheme: AppBarTheme(backgroundColor: blueish(),foregroundColor: Colors.white),
+            // primarySwatch: generateMaterialColorFromColor(Color(0xFF004AAD)),
+            // primaryColor: Colors.white,
+            backgroundColor: backgroundColor(),
+             colorScheme:ColorScheme.dark(
+              primary: Color(0xff0264e3), 
+              primaryVariant: Color(0xff5C96E1),
+              secondary: Color(0xFF646464), 
+              secondaryVariant: Color(0xff909090), 
+              surface: Color(0xff424242),
+              background: Color(0xff121212),
+              error: Color(0xffcf6679), 
+              onPrimary: Colors.white, 
+              onSecondary: Colors.black, 
+              onSurface: Colors.white, 
+              onBackground: Colors.white, 
+              onError: Colors.black, 
+              brightness: Brightness.dark,
+            )
           ),
 
           home: const MainPage(),
