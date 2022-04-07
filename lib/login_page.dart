e1c1b2795f7d1a0e84f6c7b2b06d4c82883be91b
Adiapril5 <79504399+mrgame54@users.noreply.github.com>
2022-04-07 18:04:27 +0200

    added shadow to Title and Subtitle

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,9 +7,6 @@ import 'backend_api.dart';
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
-
-
-
   @override
   State<StatefulWidget> createState() {
     return _LoginPageState();
@@ -18,9 +15,6 @@ class LoginPage extends StatefulWidget {
 
 // Here is the layout and the action triggers
 class _LoginPageState extends State<LoginPage> {
-
-
-
   // declare variables here:
   TextEditingController _usernameTextController = TextEditingController();
   TextEditingController _passwordTextController = TextEditingController();
@@ -29,167 +23,161 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController _emailTextController = TextEditingController();
   bool _showSignUp = false; //show either login screen or signup screen
 
-
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-
-          title: Image.asset(
-            'assets/icon-round.png',
-            fit: BoxFit.fitHeight,
-            height: 40,
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/icon-round.png',
+          fit: BoxFit.fitHeight,
+          height: 40,
         ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "FraPods",
+                  style: titleTextStyle(),
+                ),
 
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 8),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "FraPods",
-                    style: titleTextStyle(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  child: TextFormField(
+                    style: normalTextStyle(),
+                    controller: _usernameTextController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Username'),
                   ),
+                ),
 
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                    child: TextFormField(
-                      style: normalTextStyle(),
-                      controller: _usernameTextController,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(), labelText: 'Username'),
-                    ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  child: TextField(
+                    style: normalTextStyle(),
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    controller: _passwordTextController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Password'),
                   ),
+                ),
 
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                    child: TextField(
-                      style: normalTextStyle(),
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      controller: _passwordTextController,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(), labelText: 'Password'),
-                    ),
+                Visibility(
+                  visible: _showSignUp,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                        child: TextField(
+                          controller: _firstnameTextController,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Firstname'),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                        child: TextField(
+                          controller: _lastnameTextController,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Lastname'),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                        child: TextField(
+                          controller: _emailTextController,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'E-Mail'),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: OutlinedButton(
+                                  onPressed: () => signUp(
+                                        _usernameTextController.text.trim(),
+                                        _passwordTextController.text.trim(),
+                                        _firstnameTextController.text.trim(),
+                                        _lastnameTextController.text.trim(),
+                                        _emailTextController.text.trim(),
+                                      ),
+                                  child: const Text("Sign Up")),
+                            ),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _showSignUp = false;
+                          });
+                        },
+                        child: Text(
+                          "Already have an account? Log In",
+                          style: shadowTextStyle(),
+                        ),
+                      ),
+                    ],
                   ),
+                ),
 
-                  Visibility(
-                    visible: _showSignUp,
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding:
-                          EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                          child: TextField(
-                            controller: _firstnameTextController,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Firstname'),
-                          ),
-                        ),
-                        Padding(
-                          padding:
-                          EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                          child: TextField(
-                            controller: _lastnameTextController,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Lastname'),
-                          ),
-                        ),
-                        Padding(
-                          padding:
-                          EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                          child: TextField(
-                            controller: _emailTextController,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'E-Mail'),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 8, vertical: 8),
-                          child: Row(
-                            children: <Widget>[
-                              Expanded(
-                                child: OutlinedButton(
-                                    onPressed: () =>
-                                        signUp(
-                                          _usernameTextController.text.trim(),
-                                          _passwordTextController.text.trim(),
-                                          _firstnameTextController.text.trim(),
-                                          _lastnameTextController.text.trim(),
-                                          _emailTextController.text.trim(),
-                                        ),
-                                    child: const Text("Sign Up")),
+                Visibility(
+                  visible: !_showSignUp,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: OutlinedButton(
+                                onPressed: () => logIn(
+                                    _usernameTextController.text.toString(),
+                                    _passwordTextController.text.toString()),
+                                child: const Text("Log In"),
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                        TextButton(
+                      ),
+                      TextButton(
                           onPressed: () {
                             setState(() {
-                              _showSignUp = false;
+                              _showSignUp = true;
                             });
                           },
-                          child: Text("Already have an account? Log In"),
-                        ),
-                      ],
-                    ),
+                          child: Text(
+                            "Don't have an account? Sign Up",
+                            style: shadowTextStyle(),
+                          ))
+                    ],
                   ),
+                ),
 
-                  Visibility(
-                    visible: !_showSignUp,
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 8, vertical: 8),
-                          child: Row(
-                            children: <Widget>[
-                              Expanded(
-                                child: OutlinedButton(
-                                  onPressed: () =>
-                                      logIn(
-                                          _usernameTextController.text
-                                              .toString(),
-                                          _passwordTextController.text
-                                              .toString()
-                                      ),
-                                  child: const Text("Log In"),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        TextButton(
-                            onPressed: () {
-                              setState(() {
-                                _showSignUp = true;
-                              });
-                            },
-                            child: Text("Don't have an account? Sign Up")
-                        )
-                      ],
-                    ),
-                  ),
-
-
-                  // Layout with Sign Up button
-
-                ],
-              ),
+                // Layout with Sign Up button
+              ],
             ),
           ),
         ),
-      );
-    }
-
+      ),
+    );
+  }
 
   // Write class-internal methods here
 
@@ -203,19 +191,17 @@ class _LoginPageState extends State<LoginPage> {
       return;
     } else {
       String result = await BackendApi().logIn(username, password);
-      switch(result){
+      switch (result) {
         case "200":
-          showDialogMessage("Login: authentication", "FraPods uses 2 factor authentication. You will receive an E-Mail asking you to verify this device. Click the link and then restart this app to log in!");
+          showDialogMessage("Login: authentication",
+              "FraPods uses 2 factor authentication. You will receive an E-Mail asking you to verify this device. Click the link and then restart this app to log in!");
           break;
       }
     }
-
   }
 
-
-
-
-  Future<void> signUp(String username, String password, String firstname, String lastname, String email) async {
+  Future<void> signUp(String username, String password, String firstname,
+      String lastname, String email) async {
     if (username.isEmpty) {
       showDialogMessage("Sign Up failed", "Please enter a username!");
       return;
@@ -228,15 +214,17 @@ class _LoginPageState extends State<LoginPage> {
     } else if (lastname.isEmpty) {
       showDialogMessage("Sign Up failed", "Please enter your lastname!");
       return;
-    } else if (email.isEmpty){
+    } else if (email.isEmpty) {
       showDialogMessage("Sign Up failed", "Please enter your E-Mail address");
     } else {
       //TODO Waiting animation here (eg. circle spinning").
 
-      String result = await BackendApi().createAccount(username, password, firstname, lastname, email);
-      switch(result){
+      String result = await BackendApi()
+          .createAccount(username, password, firstname, lastname, email);
+      switch (result) {
         case "200":
-          showDialogMessage("Registration successfulf!", "The registration was successful. You can now log in!");
+          showDialogMessage("Registration successfulf!",
+              "The registration was successful. You can now log in!");
           setState(() {
             _showSignUp = false;
           });
--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ import 'login_page.dart';
 import 'setting_page.dart';
 import 'backend_api.dart';
 
-
-
 void main() {
   runApp(const FraPodsApp());
 }
@@ -19,64 +17,59 @@ void main() {
 class FraPodsApp extends StatelessWidget {
   const FraPodsApp({Key? key}) : super(key: key);
 
-
   @override
-    Widget build(BuildContext context) {
-
+  Widget build(BuildContext context) {
     BackendApi().autoLogIn();
 
     return ValueListenableBuilder<bool>(
-      valueListenable: darkNotifier, 
-      builder: (BuildContext context, bool isDark, Widget? child){
-        return MaterialApp(
-          title: 'FraPods',
-          themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
-          theme: ThemeData(
-            appBarTheme: AppBarTheme( backgroundColor: blueish(), foregroundColor: Colors.white),
-            // primaryColor: Colors.white,
-             colorScheme:ColorScheme.light(
-              primary: Color(0xff468CE8), 
-              primaryVariant: Color(0xFF99C4FD),
-              secondary: Color(0xFF424242), 
-              secondaryVariant: Color(0xff646464), 
-              surface: Color(0xffFFFEF6), 
-              background: Colors.white, 
-              error: Color(0xffb00020), 
-              onPrimary:Colors.black,
-              onSecondary: Colors.black, 
-              onSurface: Colors.black, 
-              onBackground: Colors.black, 
-              onError: Colors.white, 
-              brightness: Brightness.light,
-            )
-          ),
-
-          darkTheme: ThemeData(
-            appBarTheme: AppBarTheme(backgroundColor: blueish(),foregroundColor: Colors.white),
-            // primarySwatch: generateMaterialColorFromColor(Color(0xFF004AAD)),
-            // primaryColor: Colors.white,
-            backgroundColor: backgroundColor(),
-             colorScheme:ColorScheme.dark(
-              primary: Color(0xff0264e3), 
-              primaryVariant: Color(0xff5C96E1),
-              secondary: Color(0xFF646464), 
-              secondaryVariant: Color(0xff909090), 
-              surface: Color(0xff424242),
-              background: Color(0xff121212),
-              error: Color(0xffcf6679), 
-              onPrimary: Colors.white, 
-              onSecondary: Colors.black, 
-              onSurface: Colors.white, 
-              onBackground: Colors.white, 
-              onError: Colors.black, 
-              brightness: Brightness.dark,
-            )
-          ),
-
-          home: const LoginPage(),
-
-        );
-      });
+        valueListenable: darkNotifier,
+        builder: (BuildContext context, bool isDark, Widget? child) {
+          return MaterialApp(
+            title: 'FraPods',
+            themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+            theme: ThemeData(
+                appBarTheme: AppBarTheme(
+                    backgroundColor: blueish(), foregroundColor: Colors.white),
+                // primaryColor: Colors.white,
+                colorScheme: ColorScheme.light(
+                  primary: Color(0xff468CE8),
+                  primaryVariant: Color(0xFF99C4FD),
+                  secondary: Color(0xFF424242),
+                  secondaryVariant: Color(0xff646464),
+                  surface: Color(0xffFFFEF6),
+                  background: Colors.white,
+                  error: Color(0xffb00020),
+                  onPrimary: Colors.black,
+                  onSecondary: Colors.black,
+                  onSurface: Colors.black,
+                  onBackground: Colors.black,
+                  onError: Colors.white,
+                  brightness: Brightness.light,
+                )),
+            darkTheme: ThemeData(
+                appBarTheme: AppBarTheme(
+                    backgroundColor: blueish(), foregroundColor: Colors.white),
+                // primarySwatch: generateMaterialColorFromColor(Color(0xFF004AAD)),
+                // primaryColor: Colors.white,
+                backgroundColor: backgroundColor(),
+                colorScheme: ColorScheme.dark(
+                  primary: Color(0xff0264e3),
+                  primaryVariant: Color(0xff5C96E1),
+                  secondary: Color(0xFF646464),
+                  secondaryVariant: Color(0xff909090),
+                  surface: Color(0xff424242),
+                  background: Color(0xff121212),
+                  error: Color(0xffcf6679),
+                  onPrimary: Colors.white,
+                  onSecondary: Colors.black,
+                  onSurface: Colors.white,
+                  onBackground: Colors.white,
+                  onError: Colors.black,
+                  brightness: Brightness.dark,
+                )),
+            home: const LoginPage(),
+          );
+        });
   }
 }
 
@@ -93,7 +86,20 @@ TextStyle titleTextStyle() {
   return TextStyle(
     fontSize: 32,
     color: generateMaterialColorFromColor(Color(0xFF004AAD)),
+    shadows: <Shadow>[
+      Shadow(
+        offset: Offset(1.0, 1.0),
+        blurRadius: 1.0,
+        color: Color.fromARGB(255, 0, 0, 0),
+      ),
+      Shadow(
+        offset: Offset(1.0, 1.0),
+        blurRadius: 1.0,
+        color: Color.fromARGB(125, 0, 0, 255),
+      ),
+    ],
   );
+  ;
 }
 
 TextStyle normalTextStyle() {
@@ -110,7 +116,7 @@ TextStyle normalTextStyle2() {
   );
 }
 
-TextStyle subtitleTextStyle(){
+TextStyle subtitleTextStyle() {
   return TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.bold,
@@ -118,31 +124,47 @@ TextStyle subtitleTextStyle(){
   );
 }
 
+TextStyle shadowTextStyle() {
+  return TextStyle(
+    fontSize: 18,
+    color: generateMaterialColorFromColor(Color(0xFF004AAD)),
+    shadows: <Shadow>[
+      Shadow(
+        offset: Offset(0.5, 0.1),
+        blurRadius: 0.5,
+        color: Color.fromARGB(255, 0, 0, 0),
+      ),
+      Shadow(
+        offset: Offset(0.5, 0.1),
+        blurRadius: 0.5,
+        color: Color.fromARGB(125, 0, 0, 255),
+      ),
+    ],
+  );
+  ;
+}
+
 Color blueish() {
   return generateMaterialColorFromColor(Color(0xFF004AAD));
 }
 
-
-
-Color backgroundColor(){
+Color backgroundColor() {
   return Color(0xFF292929);
 }
 
-
 MaterialColor generateMaterialColorFromColor(Color color) {
-return MaterialColor(color.value, {
-50: Color.fromRGBO(color.red, color.green, color.blue, 0.1),
-100: Color.fromRGBO(color.red, color.green, color.blue, 0.2),
-200: Color.fromRGBO(color.red, color.green, color.blue, 0.3),
-300: Color.fromRGBO(color.red, color.green, color.blue, 0.4),
-400: Color.fromRGBO(color.red, color.green, color.blue, 0.5),
-500: Color.fromRGBO(color.red, color.green, color.blue, 0.6),
-600: Color.fromRGBO(color.red, color.green, color.blue, 0.7),
-700: Color.fromRGBO(color.red, color.green, color.blue, 0.8),
-800: Color.fromRGBO(color.red, color.green, color.blue, 0.9),
-900: Color.fromRGBO(color.red, color.green, color.blue, 1.0),
-});
+  return MaterialColor(color.value, {
+    50: Color.fromRGBO(color.red, color.green, color.blue, 0.1),
+    100: Color.fromRGBO(color.red, color.green, color.blue, 0.2),
+    200: Color.fromRGBO(color.red, color.green, color.blue, 0.3),
+    300: Color.fromRGBO(color.red, color.green, color.blue, 0.4),
+    400: Color.fromRGBO(color.red, color.green, color.blue, 0.5),
+    500: Color.fromRGBO(color.red, color.green, color.blue, 0.6),
+    600: Color.fromRGBO(color.red, color.green, color.blue, 0.7),
+    700: Color.fromRGBO(color.red, color.green, color.blue, 0.8),
+    800: Color.fromRGBO(color.red, color.green, color.blue, 0.9),
+    900: Color.fromRGBO(color.red, color.green, color.blue, 1.0),
+  });
 }
-
 
 final darkNotifier = ValueNotifier<bool>(true);
